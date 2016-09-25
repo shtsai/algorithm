7855256e10acfb3e10d7440b7563a6769da2a50f
Sean Tsai <shtsai7@gmail.com>
2016-09-24 23:49:18 -0400

    update SetMatrixZeroes.java

--- java/SetMatrixZeroes.java
+++ java/SetMatrixZeroes.java
@@ -4,7 +4,64 @@
 
 public class Solution {
     public void setZeroes(int[][] matrix) {
-        int row = matrix.length;
+	// store the states of each row in the first row, and each col in the first col
+	// uses two other variable to record if the first row and col contains 0
+	int row = matrix.length;
+	int col = matrix[0].length;
+	boolean frzero = false;
+	boolean fczero = false;    // first row zero and first col zero
+
+	for (int i = 0; i < row; i++) { // check if first row contains zero
+	    if (matrix[i][0] == 0) {
+		frzero = true;
+	    }
+	}
+
+	for (int j = 0; j < col; j++) {  // check if first col contains zero
+	    if (matrix[0][j] == 0) {
+		fczero = true;
+	    }
+	}
+
+	for (int i = 1; i < row; i++) {      // store the states in the first row and col
+	    for (int j = 1; j < col; j++) {
+		if (matrix[i][j] == 0){
+		    matrix[i][0] = 0;
+		    matrix[0][j] = 0;
+		}
+	    }
+	}
+
+	for (int i = 1; i < row; i++) {
+	    if (matrix[i][0] == 0) {
+		for (int j = 0; j < col; j++) {
+		    matrix[i][j] = 0;
+		}
+	    }
+	}
+
+	for (int j = 1; j < col; j++) {
+	    if (matrix[0][j] == 0) {
+		for (int i = 0; i < row; i++) {
+		    matrix[i][j] = 0;
+		}
+	    }
+	}
+	
+	if (frzero) {
+	    for (int i = 0; i < row; i++) {
+		matrix[i][0] = 0;
+	    }
+	}
+
+	if (fczero) {
+	    for (int j = 0; j < col; j++) {
+		matrix[0][j] = 0;
+	    }
+	}
+
+	/* This approach uses two arrays, which takes O(m+n) space
+	int row = matrix.length;
         int col = matrix[0].length;
         
         // use two bool arrays to keep track of the zero rows and cols
@@ -40,5 +97,6 @@ public class Solution {
                 }
             }
         }
+	*/
     }
 }
