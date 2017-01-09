47fda22d130719ca152490503737c9a4870581b7
Shang-Hung Tsai <shtsai7@gmail.com>
2017-01-09 22:42:02 +0800

    add LinkedListCycle.java

---
+++ java/LinkedListCycle.java
@@ -0,0 +1,33 @@
+/*
+ * Given a linked list, determine if it has a cycle in it.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+// slow and fast pointer
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
