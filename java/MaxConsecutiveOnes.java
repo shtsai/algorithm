8029da0f1245aeb26f3cf95360babd5c74579a9f
Shang-Hung Tsai <shtsai7@gmail.com>
2017-01-25 21:37:02 +0800

    add MaxConsecutiveOnes.java

---
+++ java/MaxConsecutiveOnes.java
@@ -0,0 +1,20 @@
+/*
+ * Given a binary array, find the maximum number of consecutive 1s in this array.
+ */
+
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        
+        return Math.max(max, count);
+    }
+}
