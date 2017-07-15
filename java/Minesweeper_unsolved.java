a8af3b0b054b6b473f28dd24956dbbde217f7a83
Shang-Hung Tsai <shtsai7@gmail.com>
2017-07-15 09:36:36 +0800

    add Minesweeper_unsolved.java

---
+++ java/Minesweeper_unsolved.java
@@ -0,0 +1,56 @@
+/*
+ * Let's play the minesweeper game (Wikipedia, online game)!
+ * 
+ * You are given a 2D char matrix representing the game board. 'M' represents an unrevealed mine, 'E' represents an unrevealed empty square, 'B' represents a revealed blank square that has no adjacent (above, below, left, right, and all 4 diagonals) mines, digit ('1' to '8') represents how many mines are adjacent to this revealed square, and finally 'X' represents a revealed mine.
+ * 
+ * Now given the next click position (row and column indices) among all the unrevealed squares ('M' or 'E'), return the board after revealing this position according to the following rules:
+ * 
+ * If a mine ('M') is revealed, then the game is over - change it to 'X'.
+ * If an empty square ('E') with no adjacent mines is revealed, then change it to revealed blank ('B') and all of its adjacent unrevealed squares should be revealed recursively.
+ * If an empty square ('E') with at least one adjacent mine is revealed, then change it to a digit ('1' to '8') representing the number of adjacent mines.
+ * Return the board when no more squares will be revealed.
+ */
+
+// Unsolved
+// Stack overflow
+public class Solution {
+    public char[][] updateBoard(char[][] board, int[] click) {
+        // click poisition is a mine
+        if (board[click[0]][click[1]] == 'M') {
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+        
+        // click position is empty
+        reveal(board, click);
+        return board;
+    }
+    
+    public void reveal(char[][] board, int[] click) {
+        // invalid index
+        if (click[0] < 0 || click[0] >= board.length || click[1] < 0 || click[1] >= board[0].length) return;
+        
+        int count = 0;
+        for (int i = click[0] - 1; i <= click[0] + 1; i++) {        // check adjacent row
+            for (int j = click[1] - 1; j <= click[1] + 1; j++) {    // check adjacent col
+                if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+                    continue;
+                }
+                if (board[i][j] == 'M') {
+                    count++;
+                }
+            }
+        }
+        if (count != 0) {
+            board[click[0]][click[1]] = Character.forDigit(count, 10);
+        } else {
+            board[click[0]][click[1]] = 'B';
+            for (int i = click[0] - 1; i <= click[0] + 1; i++) {        // check adjacent row
+                for (int j = click[1] - 1; j <= click[1] + 1; j++) {    // check adjacent col
+                    reveal(board, new int[]{i, j});
+                }
+            }
+        }
+        return;
+    }
+}
