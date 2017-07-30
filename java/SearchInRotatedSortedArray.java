c7090bf8abccc51f27abc24491b0050d2ae13447
Shang-Hung Tsai <shtsai7@gmail.com>
2017-07-30 17:34:17 +0800

    add SearchInRotatedSortedArray.java

---
+++ java/SearchInRotatedSortedArray.java
@@ -0,0 +1,29 @@
+/*
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * 
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * 
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * 
+ * You may assume no duplicate exists in the array.
+ */
+
+// binary search
+public class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] >= nums[i]) {       // left half is in accending order
+                if (nums[i] <= target && target <= nums[mid]) j = mid - 1;
+                else i = mid + 1;
+            } else {                               // right half is in accending order
+                if (nums[mid] <= target && target <= nums[j]) i = mid + 1;
+                else j = mid - 1;
+            }
+        }
+        return nums[i] == target ? i : -1;
+    }
+}
