ea33696149afb681e2dcd6d4c5750319de1159d2
Sean Tsai <shtsai7@gmail.com>
2017-08-31 16:57:18 -0400

    update LongestPalindromicSubstring.java

--- java/LongestPalindromicSubstring.java
+++ java/LongestPalindromicSubstring.java
@@ -15,6 +15,67 @@
  * Output: "bb"
  */
 
+// Solution 2, version 2:
+// use helper function to find Palindrome, cleaner and faster
+class Solution {
+    private int max, maxLeft;
+    
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() <= 1) return s;
+        max = 0;
+        maxLeft = 0;
+        for (int i = 0; i < s.length(); i++) {
+            findPalindrome(s, i, i);        // one char as center
+            findPalindrome(s, i, i+1);      // two chars as center
+        }
+        return s.substring(maxLeft, maxLeft+max);
+    }
+    
+    private void findPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left)==s.charAt(right)) {
+            left--;
+            right++;
+        }
+        if (max < right-left-1) {
+            max = right-left-1;
+            maxLeft = left+1;
+        }
+    }
+}
+
+// Solution 2:
+// try centering at every position, use one char or two chars as center
+// time: O(n2)
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() <= 1) return s;
+        int max = 0, maxLeft = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int oneLeft = i, oneRight = i;
+            int twoLeft = i, twoRight = i+1;
+            while (oneLeft >= 0 && oneRight < s.length() && s.charAt(oneLeft)==s.charAt(oneRight)) {
+                oneLeft--;
+                oneRight++;
+            }
+            if (max < oneRight-oneLeft-1) {
+                max = oneRight-oneLeft-1;
+                maxLeft = oneLeft+1;
+                
+            }
+            while (twoLeft >= 0 && twoRight < s.length() && s.charAt(twoLeft)==s.charAt(twoRight)) {
+                twoLeft--;
+                twoRight++;
+            }
+            if (max < twoRight-twoLeft-1) {
+                max = twoRight-twoLeft-1;
+                maxLeft = twoLeft+1;
+            }
+        }
+        return s.substring(maxLeft, maxLeft+max);
+    }
+}
+
+// Solution 1:
 // expand from the center, search for palindrome starting from every index
 // O(n2) time
 public class Solution {
