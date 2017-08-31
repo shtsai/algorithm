e3b615051515897891456eef00bf186553772674
Sean Tsai <shtsai7@gmail.com>
2017-08-31 11:36:54 -0400

    add new solution MaximalRectangle.java

--- java/MaximalRectangle.java
+++ java/MaximalRectangle.java
@@ -11,6 +11,9 @@
  * 
  */
 
+// Solution 1:
+// Dynamic programming, use arrays to store left boundary, right boundary and height
+// update max in every iteration
 class Solution {
     public int maximalRectangle(char[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
@@ -18,9 +21,8 @@ class Solution {
         int[] height = new int[n];  // record height
         int[] left = new int[n];    // record the left boundary of rectangles
         int[] right = new int[n];   // record the right boundary
-        for (int i = 0; i < n; i++) right[i] = n;
-        
-        
+        for (int i = 0; i < n; i++) right[i] = n;	// initialize right to all n
+
         for (int i = 0; i < m; i++) {
             int curLeft = 0, curRight = n;
             for (int j = 0; j < n; j++) {        // left to right
@@ -48,3 +50,40 @@ class Solution {
     }
 }
 
+// Solution 2:
+// base on "Largest Rectangle In Histogram"
+// use stacks to record left boundaries and heights
+// a little bit slower than solutio 1
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int m = matrix.length, n = matrix[0].length, max = 0;
+        int[] rectangles = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {           // create histogram
+                if (matrix[i][j] == '1') rectangles[j]++;
+                else rectangles[j] = 0;
+            }
+            
+            // Largest Rectangle In Histogram
+            Stack<Integer> stack = new Stack<>();
+            int index = 0;
+            while (index < n) {
+                if (stack.isEmpty() || rectangles[index] >= rectangles[stack.peek()]) {
+                    stack.push(index);
+                    index++;
+                } else {
+                    int height = rectangles[stack.pop()];
+                    int width = stack.isEmpty() ? index : index - (stack.peek()+1);
+                    max = Math.max(max, height * width);
+                }
+            }
+            while (!stack.isEmpty()) {
+                int height = rectangles[stack.pop()];
+                int width = stack.isEmpty() ? index : index - (stack.peek()+1);
+                max = Math.max(max, height * width);
+            }
+        }
+        return max;
+    }
+}
