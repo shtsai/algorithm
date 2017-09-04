edb593ed5d353f315dacf90bc5243a2f34740034
Shang-Hung Tsai <shtsai7@gmail.com>
2017-09-04 16:50:49 -0400

    update ClimbingStairs.java

--- java/ClimbingStairs.java
+++ java/ClimbingStairs.java
@@ -4,19 +4,39 @@
  * Note: Given n will be a positive integer.
  */
 
-//Dynamic Programming
+// Solution 2:
+// Dynamic Programming
+// only store two integers, b/c we only need the result from last two steps
+// time: O(n), space: O(1)
 public class Solution {
     public int climbStairs(int n) {
         int first = 1;
         int second = 2;
         if (n == 0 || n == 1) return 1;
-        
+
         for (int i = n - 2; i > 0; i--) {
             int steps = first + second;
             first = second;
             second = steps;
         }
-        
+
         return second;
+    }
+}
+
+// Solution 1:
+// use an array for memoization
+// time: O(n), space O(n)
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1) return 1;
+        if (n == 2) return 2;
+        int[] dp = new int[n+1];
+        dp[1] = 1;      // initialzie base cases
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
