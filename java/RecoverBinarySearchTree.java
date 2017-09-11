a94a182e44511d8e06cb5972c36edb630a1cb3f3
Shang-Hung Tsai <shtsai7@gmail.com>
2017-09-11 16:54:19 -0400

    add RecoverBinarySearchTree.java

---
+++ java/RecoverBinarySearchTree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// Solution 1:
+// First do in-order traversal to find the two misplaced nodes.
+// Then swap the value of these two nodes.
+// reference: https://discuss.leetcode.com/topic/3988/no-fancy-algorithm-just-simple-and-powerful-in-order-traversal
+
+class Solution {
+    TreeNode prev, first, second;
+
+    public void recoverTree(TreeNode root) {
+        prev = new TreeNode(Integer.MIN_VALUE);
+        first = second = null;
+        recover(root);
+
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    /* a helper function that use in-order traversal to find
+       first and second misplaced nodes */
+    private void recover(TreeNode node) {
+        if (node == null) return;
+        recover(node.left);
+
+        if (first == null && prev.val > node.val) {
+            first = prev;
+        }
+        if (first != null && prev.val > node.val) {
+            second = node;
+        }
+        prev = node;
+
+        recover(node.right);
+    }
+}
