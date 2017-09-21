5b3960f0984d42b6ec0763ea37aace12df05f2b6
Shang-Hung Tsai <shtsai7@gmail.com>
2017-09-21 17:11:30 -0400

    update ValidPalindrome.java

--- java/ValidPalindrome.java
+++ java/ValidPalindrome.java
@@ -7,6 +7,40 @@
  * "race a car" is not a palindrome.
  */
 
+// Solution 2: 
+// Sightly cleaner version
+// Uses built-in Character functions isLetterOrDigit() and toLowerCase()
+// Instead of converting whole string to lower case, convert only when
+// compare characters
+//
+// Time: O(n)
+// Space: O(1)
+// 
+// -- 09/21/2017
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() <= 1) return true;
+        int left = 0, right = s.length()-1;
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(left++)) != Character.toLowerCase(s.charAt(right--))) return false;
+        }
+        return true;
+    }
+}
+
+// Solution 1: Two pointers
+// First convert whole string to lower case.
+// Then use two pointers head and tail.
+// Skip all the invalid chars, and do the comparison.
 public class Solution {
     public boolean isPalindrome(String s) {
         if (s == null || s.length() == 0) return true;
