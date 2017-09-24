f9dd62f57c2ef57fed8ee507fa80dc7a980a7448
Shang-Hung Tsai <shtsai7@gmail.com>
2017-09-24 11:30:42 -0400

    add ReadNCharactersGivenRead4.java

---
+++ java/ReadNCharactersGivenRead4.java
@@ -0,0 +1,43 @@
+/*
+ * The API: int read4(char *buf) reads 4 characters at a time from a file.
+ * 
+ * The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
+ * 
+ * By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+ * 
+ * Note:
+ * The read function will only be called once for each test case. 
+ */
+
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+// Solution 1: 
+// reference: https://discuss.leetcode.com/topic/18289/another-accepted-java-solution
+// 09/24/2017
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        boolean eof = false;  // a flag indicating eof
+        int count = 0;
+        char[] newbuf = new char[4];
+        
+        while (n - count > 0 && !eof) {
+            int newRead = read4(newbuf);
+            if (newRead < 4) {  // reach end of file
+                eof = true;
+            }
+            int toWrite = Math.min(n-count, newRead); // get the # of chars to write
+            for (int i = 0; i < toWrite; i++) { // write the chars to buffer
+                buf[count] = newbuf[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
