2fbefb77533009953cb86acc555f1a29a626672e
Sean Tsai <shtsai7@gmail.com>
2017-09-25 21:16:33 -0400

    update RomanToInteger.java

--- java/RomanToInteger.java
+++ java/RomanToInteger.java
@@ -3,6 +3,38 @@
  * Input is guaranteed to be within the range from 1 to 3999.
  */
 
+
+// Solution 1 version 2:
+// A more consice version
+// Store roman -> integer mappings in a hashmap
+// When a smaller value appears before a larger value, we need to subtract it
+// Else, just add the value to the result
+// Time: O(n)
+// Space: O(1)
+// 09/25/2017
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i+1 < s.length() && map.get(s.charAt(i))<map.get(s.charAt(i+1))) {
+                res -= map.get(s.charAt(i));
+            } else {
+                res += map.get(s.charAt(i));
+            }
+        }
+        return res;
+    }
+}
+
+// Solution 1:
 public class Solution {
     public int romanToInt(String s) {
 
