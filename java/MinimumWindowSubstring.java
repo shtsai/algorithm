09159b7666807c9bef01ad6274430e9519156315
Sean Tsai <shtsai7@gmail.com>
2017-09-27 11:51:07 -0400

    update MinimumWindowSubstring.java

--- java/MinimumWindowSubstring.java
+++ java/MinimumWindowSubstring.java
@@ -12,8 +12,60 @@
  * If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
  */
 
-// https://discuss.leetcode.com/topic/30941/here-is-a-10-line-template-that-can-solve-most-substring-problems/12
-// Map and two pointers
+// Solution 1: version 2
+// HashMap version
+// Time: O(n) - n = s.length()
+// Space: O(n)
+// 09/27/2017
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int count = t.length();
+        for (int i = 0; i < t.length(); i++) {  // store char frequency of t
+            char c = t.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c)-1);
+            } else {
+                map.put(c, -1);
+            }
+        }
+        
+        int left = 0, right = 0;
+        int min = Integer.MAX_VALUE, minLeft = 0, minRight = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) {
+                if (map.get(c) < 0) {
+                    count--;
+                }
+                map.put(c, map.get(c)+1);
+                
+            }
+            right++;
+            while (left < right && count == 0) {   // find all chars
+                if ((right - left) < min) { // update min
+                    min = right - left;
+                    minLeft = left;
+                    minRight = right;
+                }
+                c = s.charAt(left);
+                if (map.containsKey(c)) {
+                    if (map.get(c) == 0) {  // find a new valid start
+                        count++;
+                    }
+                    map.put(c, map.get(c)-1);
+                }
+                left++;
+            }
+        }
+        
+        return s.substring(minLeft, minRight);
+    }
+}
+
+// Solution 1: Map and two pointers
+// reference: https://discuss.leetcode.com/topic/30941/here-is-a-10-line-template-that-can-solve-most-substring-problems/12
+
 public class Solution {
     public String minWindow(String s, String t) {
         int[] map = new int[128];
