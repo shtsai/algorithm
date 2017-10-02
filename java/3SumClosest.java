9e759f25cd1d740c1d5645a4ba7eaf0bd71fe62b
Sean Tsai <shtsai7@gmail.com>
2017-10-02 11:18:16 -0400

    update 3SumClosest.java

--- java/3SumClosest.java
+++ java/3SumClosest.java
@@ -5,33 +5,35 @@
  * You may assume that each input would have exactly one solution.
  */
 
-public class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        // similar to 3Sum, convert the problem into 2Sum
+// Solution 1:  convert the problem into 2Sum
+// First sort the array in ascending order
+// Use a pointer to scan through the array and set goals
+// Use another two pointers to do two sum searches
+// Update closest and diff when we find a closer pair
+// Time: O(n^2)
+// Space: O(1)
+// 10/02/2017
 
-        int delta = Integer.MAX_VALUE;
-        int sum = 0;
-        
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int closest = Integer.MAX_VALUE, diff = Integer.MAX_VALUE;
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
-            int smallTarget = target - nums[i];
-            while (j < k) {
-                int temp = nums[i] + nums[j] + nums[k];
-                if (temp == target) return temp;
-                
-                if (delta > Math.abs(temp-target)) {
-                    sum = nums[i]+nums[j]+nums[k];
-                    delta = Math.abs(temp-target);
+        for (int i = 0; i < nums.length; i++) {
+            int goal = target - nums[i];
+            int left = i+1, right = nums.length-1;
+            while (left < right) {
+                if (nums[left]+nums[right] == goal) return target;
+                if (Math.abs(nums[left]+nums[right]-goal) < diff) { // find a closer one
+                    closest = nums[left] + nums[right] + nums[i];
+                    diff = Math.abs(nums[left]+nums[right]-goal);
                 }
-                if (temp > target) {
-                    k--;
+                if (nums[left]+nums[right] < goal) {  // continue search
+                    left++;
                 } else {
-                    j++;
+                    right--;
                 }
             }
         }
-        return sum;
+        return closest;
     }
 }
