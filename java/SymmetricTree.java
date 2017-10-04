7812a96f761e0ff481393f0f3035d19597bced68
Shang-Hung Tsai <shtsai7@gmail.com>
2017-10-04 18:20:49 -0400

    update SymmetricTreeNode.java

--- java/SymmetricTree.java
+++ java/SymmetricTree.java
@@ -12,8 +12,50 @@
  * }
  */
 
+// Solution 2: iterative solution
+// Use a stack to store the nodes we need compare
+// 10/04/2017
 
-// recursive solution
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        if (root.left != null) {
+            if (root.right == null) return false;
+            stack.push(root.left);
+            stack.push(root.right);
+        } else if (root.right != null) {
+            return false;
+        }
+        
+        while (!stack.isEmpty()) {
+            TreeNode right = stack.pop();
+            TreeNode left = stack.pop();
+            if (right.val != left.val) return false;
+            if (left.left != null) {
+                if (right.right == null) return false;
+                stack.push(left.left);
+                stack.push(right.right);
+            } else if (right.right != null) return false;
+            
+            if (left.right != null) {
+                if (right.left == null) return false;
+                stack.push(left.right);
+                stack.push(right.left);
+            } else if (right.left != null) return false;
+        }
+        return true;
+    }
+}
+
+// Solution 1: recursive solution
+// Check a few cases:
+//      1. if root is null, return true
+//      2. if left and right don't have the same value, or at least one of them is null
+//         return false
+//      3. if left and right have the same value, recursively check their children
+// reference:https://discuss.leetcode.com/topic/5941/recursive-and-non-recursive-solutions-in-java
+// 10/04/2017
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
@@ -26,6 +68,7 @@ public class Solution {
         }
         
         if (left.val != right.val) return false;
+        // key step
         return helper(left.left, right.right) && helper(left.right, right.left);    //recursively check the next level
         
     }
