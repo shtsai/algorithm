3263b6dcfc9b779ef40b5e871652ae04f045118d
Shang-Hung Tsai <shtsai7@gmail.com>
2017-10-08 09:51:36 -0400

    add StrobogrammaticNumberIII.java

---
+++ java/StrobogrammaticNumberIII.java
@@ -0,0 +1,67 @@
+/*
+    A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+    Write a function to count the total strobogrammatic numbers that exist in the range of low <= num <= high.
+
+    For example,
+    Given low = "50", high = "100", return 3. Because 69, 88, and 96 are three strobogrammatic numbers.
+
+    Note:
+    Because the range might be a large number, the low and high numbers are represented as string.
+ */
+
+// Solution 1:
+// Based on Strobogrammatic Number II
+// Generate all strobogrammatic Number with length between
+// low.length() and high.length(), then count the number of 
+// valid ones.
+// Time: O(m*(m-n))=O(m^2) - m:high.length, n:low.length
+// Space: O(m^2)
+// 10/08/2017
+
+class Solution {
+    String[][] pairs = {{"1","1"},{"6","9"},{"9","6"},{"8","8"}};
+    public int strobogrammaticInRange(String low, String high) {
+        List<String> nums = new ArrayList<>();
+        
+        for (int len = low.length(); len <= high.length(); len++) {
+            nums.addAll(helper(len, len));
+        }
+        
+        int count = 0;
+        for (String s : nums) {
+            if (s.length() == low.length() && s.compareTo(low) < 0) {
+                continue;
+            }
+            if (s.length() == high.length() && s.compareTo(high) > 0) {
+                continue;
+            }
+            count++;
+        }
+        return count;
+    }
+    
+    private List<String> helper(int cur, int max) {
+        List<String> res = new ArrayList<>();
+        if (cur == 0) {
+            res.add("");
+            return res;
+        }
+        if (cur == 1) {
+            res.add("0");
+            res.add("1");
+            res.add("8");
+            return res;
+        }
+        List<String> sublist = helper(cur-2, max);
+        for (String sub : sublist) {
+            if (cur != max) {
+                res.add("0" + sub + "0");
+            }
+            for (String[] pair : pairs) {
+                res.add(pair[0] + sub + pair[1]);
+            }
+        }
+        return res;
+    }
+}
