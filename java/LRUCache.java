7f6305dd59b8b40a0ae7047149589f9292a1a2e0
Sean Tsai <shtsai7@gmail.com>
2017-10-16 08:23:27 -0400

    update LRUCache.java

--- java/LRUCache.java
+++ java/LRUCache.java
@@ -14,6 +14,90 @@
  * obj.put(key,value);
  */
 
+// Solution 1 version 2:
+// More natural implementation: add to tail, remove from head (like a queue)
+// Also evict before insert, satisfy requirement
+// Time: O(1) put, O(1) get
+// Space: O(n) - n = capacitiy
+// 10/16/2017
+
+class LRUCache {
+    private class Node {
+        int key;
+        int val;
+        Node pre;
+        Node next;
+        public Node(int key, int value) {
+            this.key = key;
+            this.val = value;
+        }
+    }
+    
+    HashMap<Integer, Node> map;
+    Node head;
+    Node tail;
+    int capacity;
+    int size;
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        map = new HashMap<>();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.pre = head;
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        Node p = map.get(key);
+        remove(p);
+        addToTail(p);
+        return p.val;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) { // update key
+            Node toUpdate = map.get(key);
+            toUpdate.val = value;
+            remove(toUpdate);
+            addToTail(toUpdate);
+        } else { // need insert
+            if (size == capacity) { // evict before insert
+                evict();
+            }
+            if (size < capacity) {  // check have enough room
+                Node newNode = new Node(key, value);
+                map.put(key, newNode);
+                addToTail(newNode);
+                size++;
+            }
+        }
+    }
+    
+    private void remove(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+    
+    private void addToTail(Node node) {
+        node.pre = tail.pre;
+        node.next = tail;
+        node.pre.next = node;
+        node.next.pre = node;
+    }
+    
+    private void evict() {
+        Node toRemove = head.next;
+        if (toRemove == tail) return;   // cannot remove dummy tail
+        remove(toRemove);
+        map.remove(toRemove.key);
+        size--;
+    }
+}
+
+// Solution 1:
 // Use a doubly-linked list, add and delete are O(1)
 // Use HashMap to enable quick find for a node
 // reference: https://discuss.leetcode.com/topic/6613/java-hashtable-double-linked-list-with-a-touch-of-pseudo-nodes
