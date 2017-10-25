23fc515c8327cbff5222059b30792ef5292fded3
Shang-Hung Tsai <shtsai7@gmail.com>
2017-10-24 23:08:12 -0400

    update LinkedListCycle.java

--- java/LinkedListCycle.java
+++ java/LinkedListCycle.java
@@ -1,5 +1,8 @@
 /*
- * Given a linked list, determine if it has a cycle in it.
+    Given a linked list, determine if it has a cycle in it.
+
+    Follow up:
+    Can you solve it without using extra space?
  */
 
 /**
@@ -13,7 +16,16 @@
  *     }
  * }
  */
+
+// Solution 2:
 // slow and fast pointer
+// Scan the list with two pointers.
+// If two pointers meet, there must exists a cycle.
+// Time: O(j + k) = O(n) - j is the length of the non-cyclic part
+//                         k is the length of cycle
+// Space: O(1)
+// 10/24/2017
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null) return false;
@@ -28,6 +40,27 @@ public class Solution {
                 return true;
             }
         }
+        return false;
+    }
+}
+
+// Solution 1: HashSet
+// Use a hashSet to keep track of the nodes that we have visited
+// Time: O(n) - n is the length of the list
+// Space: O(n)
+// 10/24/2017
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        
+        ListNode p = head;
+        while (p != null) {
+            if (set.contains(p)) return true;
+            else set.add(p);
+            p = p.next;
+        }
+        
         return false;
     }
 }
