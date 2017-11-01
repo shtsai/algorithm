840ea7e89fc657646fb0a679efd4678dd30d1f92
Shang-Hung Tsai <shtsai7@gmail.com>
2017-10-31 23:54:57 -0400

    update BinaryTreeLevelOrderTraversal.java

--- java/BinaryTreeLevelOrderTraversal.java
+++ java/BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,19 @@
 /*
- * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+        Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+        For example:
+        Given binary tree [3,9,20,null,null,15,7],
+            3
+           / \
+          9  20
+            /  \
+           15   7
+        return its level order traversal as:
+        [
+          [3],
+          [9,20],
+          [15,7]
+        ] 
  */
 
 /**
@@ -11,6 +25,41 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+// Solution 2: DFS
+// Use DFS with a parameter level.
+// level will indicates the index of the list to which the node should be added
+// Recursively add each node to the list of its level.
+// Time: O(n)
+// Space: O(logn) - call stack
+// 10/31/2017
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        dfs(root, res, 0);
+        return res;
+    }
+    
+    private void dfs(TreeNode root, List<List<Integer>> res, int level) {
+        if (root == null) return;
+        if (level >= res.size()) {
+            res.add(new ArrayList<>());
+        }
+        res.get(level).add(root.val);
+        dfs(root.left, res, level+1);
+        dfs(root.right, res, level+1);
+    }
+}
+
+// Solution 1: BFS
+// Use a queue to perform BFS.
+// Needs to keep track of number of nodes at each level and next level.
+// Time: O(n) - scan each node exactly once
+// Space: O(n) - queue
+// 10/31/2017
+
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
