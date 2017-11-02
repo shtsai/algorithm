3bd1cb067b468097bd9d868e18aa0c25ae5476c9
Shang-Hung Tsai <shtsai7@gmail.com>
2017-11-02 12:00:08 -0400

    update FirstBadVersion.java

--- java/FirstBadVersion.java
+++ java/FirstBadVersion.java
@@ -14,22 +14,31 @@
       boolean isBadVersion(int version); */
 
 
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+// Solution 1: Binary Search
+// Perform Binary search to repeatedly check the middle version.
+// If the middle version is bad, we know all subsequent versions must
+// also be bad, so we continue searching on the left half.
+// If the middle version is good, we know all previous versions must
+// be good, so we continue searching on the right half.
+//
+// Time: O(logn)
+// Space: O(1)
+// 11/02/2017
+      
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int begin = 1;
-        int last = n;
-        
-        while (begin < last) {
-        	// use following approach to calculate middle to avoid overflow
-            int middle = begin + (last-begin) / 2;
-            if (isBadVersion(middle)) {
-                last = middle;
+        int left = 0, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
             } else {
-                begin = middle + 1;
+                left = mid + 1;
             }
         }
-        
-        return begin;
-        
+        return left;
     }
 }
