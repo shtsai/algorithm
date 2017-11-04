c896ef4c95c054efab69a8211eb12441c9cc8137
Shang-Hung Tsai <shtsai7@gmail.com>
2017-11-04 13:12:11 -0400

    update HouseRobberII.java

--- java/HouseRobberII.java
+++ java/HouseRobberII.java
@@ -9,6 +9,45 @@
  * determine the maximum amount of money you can rob tonight without alerting the police.
  */
 
+// Solution 1 version 2:
+// Since the houses now are placed in a circular manner,
+// we need to perform two dp operations, one including the 
+// first house and excluding the last house, and one including
+// the last house and excluding the first house.
+// This will give us the optimal result.
+//
+// Time: O(n) - two passes
+// Space: O(1) - two varibles
+// 11/04/2017
+
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        
+        int two = nums[0];
+        int one = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length-1; i++) {   // rob first house
+            int max = Math.max(one, two + nums[i]);
+            two = one;
+            one = max;
+        }
+        int res = one;
+        
+        two = 0;
+        one = nums[1];
+        for (int i = 2; i < nums.length; i++) {     // don't rob first house
+            int max = Math.max(one, two + nums[i]);
+            two = one;
+            one = max;
+        }
+        return Math.max(res, one);
+    }
+}
+
+// Solution 1 version 1:
+// Time: O(n) - two passes
+// Space: O(n) - dp array
 public class Solution {
     public int rob(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
