8b13ea3cb335b71b385d7da1f3910a708a5b9bdf
Shang-Hung Tsai <shtsai7@gmail.com>
2017-11-06 09:51:49 -0500

    update RemoveInvalidParentheses.java

--- java/RemoveInvalidParentheses.java
+++ java/RemoveInvalidParentheses.java
@@ -10,6 +10,69 @@
  */
 
 // reference: https://discuss.leetcode.com/topic/34875/easy-short-concise-and-fast-java-dfs-3-ms-solution
+
+// Solution 1 version 2: DFS
+// First check from left to right.
+// Use a counter, +1 for '(', -1 for ')'.
+// When the counter falls below zero, we know we have more ')' than '(', so we need to delete '('.
+// Start searching for '(' to delete from the last delete index to our last search index
+// remove the '(' and recursively call the remover function to continue removing.
+// For the recursively calls, they only need to continue searching from the last search index
+// and deleting from last delete index.
+// Also note that we need to handle duplicates. When multiple ')'s occur, we only handle
+// the first one. All subsequent ')'s are ignored.
+// After we check from left the right, we also need to check from right to left.
+//
+// Time: O(n) - at most n chars to delete
+// Space: O(1)
+// 11/06/2017
+
+class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> res = new ArrayList<>();
+        remover(s, res, 0, 0, false);
+        return res;
+    }
+    
+    private void remover(String s, List<String> res, int searchIndex, int lastDelete, boolean rightToLeft) {
+        char L = '(';
+        char R = ')';
+        if (rightToLeft) {
+            L = ')';
+            R = '(';
+        }   
+        int counter = 0;
+        for (int i = searchIndex; i < s.length(); i++) {
+            if (s.charAt(i) == L) {
+                counter++;
+            } else if (s.charAt(i) == R) {
+                counter--;
+            }
+            
+            if (counter < 0) {   // more ')' than '(', need to delete
+                for (int j = lastDelete; j <= i; j++) {
+                    if (s.charAt(j) == R) {
+                        if (j - 1 >= lastDelete && s.charAt(j-1) == R) {
+                            continue;
+                        }
+                        StringBuilder sb = new StringBuilder(s);
+                        sb.deleteCharAt(j);
+                        remover(sb.toString(), res, i, j, rightToLeft);
+                    }
+                }
+                return;
+            }
+        }
+        if (!rightToLeft) {
+            remover(new StringBuilder(s).reverse().toString(), res, 0, 0, true);
+        }
+        if (counter == 0 && rightToLeft) {
+            res.add(new StringBuilder(s).reverse().toString());
+        }
+    }
+}
+
+// Solution 1 version 1:
 class Solution {
     public List<String> removeInvalidParentheses(String s) {
         List<String> res = new ArrayList<>();
