83a735ffc33cae5e0458bc25f0293ae32ddf2be0
Shang-Hung Tsai <shtsai7@gmail.com>
2017-11-06 23:27:01 -0500

    update WordBreak.java

--- java/WordBreak.java
+++ java/WordBreak.java
@@ -11,20 +11,26 @@
  * The wordDict parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.
  */
 
-// Solution 2:
-// dynamic programming
-// use array to memoize results of smaller subproblems
+// Solution 2: Dynamic programming
+// Use array to memoize results of smaller subproblems.
+// Each cell i represent whether we can break substring (0, i].
+// So we need to look back, and find substring (0, j] which is breakable.
+// We check if (j, i] is in the dictionary. If so, (0, i] is also breakable.
+// Otherwise, continue searching
+// 
+// Time: O(n^2)
+// Space: O(n)
+// 11/06/2017
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet<String> dict = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length()+1];	// dp[i] = true if string[0:i] is valid
-        dp[0] = true;
+        Set<String> dict = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;   // empty string
         for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                // check dp array before check dict b/c access array is faster
-                if (dp[j] && dict.contains(s.substring(j,i))) {
+            for (int j = 0; j <= i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
                     dp[i] = true;
-                    continue;   // substring(0, i) is valid
+                    break;
                 }
             }
         }
@@ -32,9 +38,42 @@ class Solution {
     }
 }
 
-// Solution 1:
-// solve recursively
-// time limit exceeded O(n2) time
+// Solution 2: BFS
+// Given a start index, try to go as far as possible using the words
+// in the dictionary. Returns true when we reach the end of the s.
+// Time: O(n^2)
+// Space: O(n)
+// 11/06/2017
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>();
+        for (String n : wordDict) {
+            dict.add(n);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        boolean[] visited = new boolean[s.length()];
+        
+        while (!q.isEmpty()) {
+            int start = q.poll();
+            if (start == s.length()) return true;
+            if (!visited[start]) {
+                for (int end = start+1; end <= s.length(); end++) {
+                    if (dict.contains(s.substring(start, end))) {
+                        q.offer(end);       // continue searching
+                    }
+                }
+            }
+            visited[start] = true;
+        }
+        return false;
+    }
+}
+
+// Solution 1: DFS (brute force)
+// Recursion and backtracking
+// Time: O(n^n) - time limit exceeded 
+// Space: O(n) - worst case call stack 
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         if (s.equals("") || s.length() == 0) return true;
