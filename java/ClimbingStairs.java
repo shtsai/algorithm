fee2df5f1fe9e53ac8f0ee0a8cfc79a67815b38f
Shang-Hung Tsai <st3127@nyu.edu>
2017-12-29 08:12:43 -0500

    update ClimbingStairs.java

--- java/ClimbingStairs.java
+++ java/ClimbingStairs.java
@@ -4,28 +4,28 @@
  * Note: Given n will be a positive integer.
  */
 
-// Solution 2:
-// Dynamic Programming
+// Solution 4:
+// Dynamic Programming, bottom-up solution
 // only store two integers, b/c we only need the result from last two steps
-// time: O(n), space: O(1)
-public class Solution {
+// Time: O(n)
+// Space: O(1)
+// Time: 12/29/2017
+class Solution {
     public int climbStairs(int n) {
-        int first = 1;
-        int second = 2;
-        if (n == 0 || n == 1) return 1;
-
-        for (int i = n - 2; i > 0; i--) {
-            int steps = first + second;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int first = 1, second = 2;
+        for (int i = 3; i <= n; i++) {
+            int sum = first + second;
             first = second;
-            second = steps;
+            second = sum;
         }
-
         return second;
     }
 }
 
-// Solution 1:
-// use an array for memoization
+// Solution 3:
+// Iterative solution with memoization
 // time: O(n), space O(n)
 class Solution {
     public int climbStairs(int n) {
@@ -38,5 +38,37 @@ class Solution {
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
+    }
+}
+
+// Solution 2: Recursive solution with memoization
+// Time: O(n)
+// Space: O(n) - memo array
+// 12/29/2017
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1) return 1;
+        int[] memo = new int[n + 1];
+        memo[1] = 1;
+        memo[2] = 2;
+        return climb(n, memo);
+    }
+    
+    public int climb(int n, int[] memo) {
+        if (memo[n] != 0) return memo[n];
+        memo[n] = climb(n-2, memo) + climb(n-1, memo);
+        return memo[n];
+    }
+}
+
+// Solution 1: Recursive solution, top down
+// Time: O(2^n)
+// Space: O(n) - call stack
+// 12/29/2017
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        return climbStairs(n-2) + climbStairs(n-1);
     }
 }
