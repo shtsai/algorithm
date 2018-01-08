5b632be883d95a68027e3a1add33aa30a4749cc5
Shang-Hung Tsai <st3127@nyu.edu>
2018-01-08 09:54:00 -0500

    update DecodeWays.java

--- java/DecodeWays.java
+++ java/DecodeWays.java
@@ -13,7 +13,7 @@
  * The number of ways decoding "12" is 2.
  */
 
-// Solution 2: Dynamic programming
+// Solution 4: Bottom-up Dynamic programming
 // Each DP[i] represents # of decode ways from index 0 to index i.
 // Recurence relation:
 //     1. if s[i] is a valid integer, 
@@ -23,7 +23,7 @@
 //
 // Time: O(n) - one pass
 // Space: O(n) - dp array
-// version 3: cleaner
+// 01/08/2018
 public class Solution {
     public int numDecodings(String s) {
         if (s == null || s.length() == 0) return 0;
@@ -41,7 +41,39 @@ public class Solution {
     }
 }
 
-// version 2: Recursive approach
+// Solution 3: Recursion with memoization, top down DP [suffix]
+// Same idea as solution 2, but use memo array to store intermediate results.
+// Time: O(n) - n problems in total
+// Space: O(n) - memo array
+// 01/08/2018
+class Solution {
+    public int numDecodings(String s) {
+        if (s.length() == 0) return 0;
+        int[] memo = new int[s.length()];
+        Arrays.fill(memo, -1);
+        return search(s, 0, memo);
+    }
+    
+    public int search(String s, int index, int[] memo) {
+        if (index == s.length()) {
+            return 1;
+        }
+        if (memo[index] != -1) return memo[index];
+        memo[index] = 0;
+        if (s.charAt(index) != '0') {
+            memo[index] += search(s, index+1, memo);
+        }
+        if (index < s.length()-1) {
+            int two = Integer.parseInt(s.substring(index, index + 2));
+            if (two >= 10 && two <= 26) {
+                memo[index] += search(s, index + 2, memo);
+            }
+        }
+        return memo[index];
+    }
+}
+
+// version 2: Suffix DP
 // 11/18/2017
 class Solution {
     public int numDecodings(String s) {
@@ -64,50 +96,60 @@ class Solution {
     }
 }
 
-// version 1:
-// 11/18/2017
+// Solution 2: Recursion
+// Break problems into smaller subproblems
+// decode[i:] = (i is valid) * decode[i+1:] + ([i:i+1] is valid) * decode[i+2:]
+// Time: O(2^n) - time limit exceeded
+// Space: O(n) - call stack
+// 01/08/2018
 class Solution {
     public int numDecodings(String s) {
         if (s.length() == 0) return 0;
-        int[] dp = new int[s.length()];
-        if (s.charAt(0) != '0') dp[0] = 1;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) != '0') {
-                dp[i] = dp[i-1];
-            }
-            if (s.charAt(i-1) != '0') {
-                int two = Integer.parseInt(s.substring(i-1, i+1));
-                if (two > 0 && two <= 26) {
-                    int preTwo = i-2 >= 0 ? dp[i-2] : 1;
-                    dp[i] += preTwo;
-                }       
+        return search(s, 0);
+    }
+    
+    public int search(String s, int index) {
+        if (index == s.length()) {
+            return 1;
+        }
+        int res = 0;
+        if (s.charAt(index) != '0') {
+            res += search(s, index+1);
+        }
+        if (index < s.length()-1) {
+            int two = Integer.parseInt(s.substring(index, index + 2));
+            if (two >= 10 && two <= 26) {
+                res += search(s, index + 2);
             }
         }
-        return dp[s.length()-1];
+        return res;
     }
 }
 
-// Solution 1: recursion
-// Time limit exceeded
-public class Solution {
+// Solution 1: Brute force, DFS search
+// Time: O(2^n) - Time limit exceeded
+// Space: O(n) - call stack
+class Solution {
+    int count;
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0) return 0;
-        return helper(s);
+        if (s.length() == 0 ) return 0;
+        count = 0;
+        search(s, 0);
+        return count;
     }
-    
-    public int helper(String s) {
-        int len = s.length(), res = 0;
-        if (s == null || len == 0) return 1;    // reaches the end, find a way of decoding
-        if (s.charAt(0) == '0') return 0;   // message cannot start with 0
-        
-        for (int i = 1; i <= s.length(); i++) {
-            int v = Integer.valueOf(s.substring(0, i));
-            if (v > 26) break;      // no mapping exist
-            if (v > 0 && v <= 26) {
-                res += helper(s.substring(i, len));     // recursively solve subproblems
-            }
-                
+    public void search(String s, int index) {
+        if (index == s.length()) {
+            count++;
+            return;
         }
-        return res;
+        if (s.charAt(index) != '0') {
+            search(s, index+1);
+        }
+        if (index < s.length()-1) {
+            int two = Integer.parseInt(s.substring(index, index + 2));
+            if (two >= 10 && two <= 26) {
+                search(s, index + 2);
+            }
+        }
     }
 }
