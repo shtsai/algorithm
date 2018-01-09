9d2068c9b62f2b54e8c969627615b6fe3c396161
Shang-Hung Tsai <st3127@nyu.edu>
2018-01-09 12:21:33 -0500

    update ReverseLinkedListII.java

--- java/ReverseLinkedListII.java
+++ java/ReverseLinkedListII.java
@@ -20,7 +20,38 @@
  * }
  */
 
-// Solution 1:
+// Solution 3: 
+// Four pointers:
+//      - dummy: head of the list
+//      - pre: node at index m - 1, right before reverse
+//      - start: node at index m, first node in the reverse part
+//      - next: a moving pointer that moves toward index n
+//
+// Time: O(n) - one pass
+// Space: O(1)
+// reference: https://discuss.leetcode.com/topic/8976/simple-java-solution-with-clear-explanation
+// 01/09/2018
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(-1), pre = dummy;
+        dummy.next = head;
+        for (int i = 0; i < m - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode start = pre.next, next = start.next;
+        
+        for (int i = 0; i < n - m; i++) {
+            start.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+            next = start.next;
+        }
+        
+        return dummy.next;
+    }
+}
+
+// Solution 2:
 // Use a dummy head to hold the linked list.
 // Find the start and end point of the reverse part, and do the reverse.
 // Then connect the reversed portion with the original list.
@@ -52,5 +83,48 @@ class Solution {
             index++;
         }
         return dummy.next;
+    }
+}
+
+
+// Solution 1:
+// First break the list at m and n.
+// Then reverse from m to n.
+// Finally connect them back with original list.
+// Time: O(n)
+// Space: O(1)
+// 01/09/2018
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(-1), p = dummy, prev = null;
+        dummy.next = head;
+        int i = 0;
+        while (i < m) {
+            prev = p;
+            p = p.next;
+            i++;
+        }
+        ListNode rStart = p;       // find reverse start
+        while (i < n) {
+            p = p.next;
+            i++;
+        }
+        ListNode rEnd = p, remaining = p.next;  // find reverse end
+        prev.next = null;       // break the list at m and n
+        rEnd.next = null;
+        reverse(rStart);
+        prev.next = rEnd;       // reconnect them
+        rStart.next = remaining;
+        return dummy.next;
+    }
+    
+    public void reverse(ListNode node) {
+        ListNode prev = null;
+        while (node != null) {
+            ListNode next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
     }
 }
