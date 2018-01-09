3bea687c685b4ff6944674269297deb108962db3
Shang-Hung Tsai <shtsai7@gmail.com>
2018-01-09 16:36:36 -0500

    update WildcardMatching.java

--- java/WildcardMatching.java
+++ java/WildcardMatching.java
@@ -18,6 +18,55 @@
  * isMatch("aab", "c*a*b") → false
  */
 
+// Solution 2: Button-up DP
+// Put two string s and p on the two sides of a matrix.
+// DP[i][j] represent whether str1[0:i] matches str2[0:j]
+// Compare each pair of characters str1[i] and str2[j].
+//      1. Two chars are the same OR '?'
+//              result depends on DP[i-1][j-1]
+//      2. '*'
+//              - treat it as an empty string => DP[i][j-1]
+//              - treat it as the matching char => DP[i-1][j-1]
+//              - treat it as an extending char => DP[i-1][j]
+//      3. Mismatch, return false
+//
+// Time: O(mn)
+// Space: O(mn)
+// 01/09/2018
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= p.length(); i++) {
+            dp[0][i] = dp[0][i-1] && p.charAt(i-1) == '*';
+        }
+        
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (p.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i-1][j-1] || dp[i-1][j] || dp[i][j-1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        
+        return dp[s.length()][p.length()];
+    }
+}
+
+// Solution 1: 
+// Scan through two strings using two pointers, compare char by char.
+// When see a '*', recording its position j and the matching index i.
+// When find mismatch, return to that star index and retry.
+// For each retry, increment i by 1.
+// Finally, check whether we have use all chars in p. If so, return true.
+// Time: O(mn)
+// Space: O(1)
+// 01/09/2018
 public class Solution {
     public boolean isMatch(String s, String p) {
         int i = 0;
