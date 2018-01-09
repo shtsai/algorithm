ee4c7a6a7981585a4ae15e92ae052fece107ea65
Shang-Hung Tsai <st3127@nyu.edu>
2018-01-09 11:22:08 -0500

    update ReverseLinkedList.java

--- java/ReverseLinkedList.java
+++ java/ReverseLinkedList.java
@@ -2,16 +2,59 @@
  * Reverse a singly linked list.
  */
 
-public class Solution {
+// Solution 3: in-place
+// Time: O(n)
+// Space: O(1)
+// 01/09/2018
+class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode end = null;     // the end should be null, end of the list
-        while (head != null) {
-            ListNode next = head.next;   // next stores the next node
-            head.next = end;             // let head points to end
-            end = head;                  // move end the next node
-            head = next;                 // move head to next node
+        ListNode p = head, prev = null;
+        while (p != null) {
+            ListNode next = p.next;
+            p.next = prev;
+            prev = p;
+            p = next;
         }
-        
-        return end;     // return end, because head is null at this point
+        return prev;
     }
 }
+
+// Solution 2: Recursion
+// Recursively reverse the remaining list, then append current node at the end.
+// This can be done by doing (head.next.next = head)
+// Draw it on a piece of paper to understand it
+// Time: O(n)
+// Space: O(n) - stack
+// 01/09/2018
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode reversed = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return reversed;
+    }
+}
+
+// Solution 1: Stack
+// Push all nodes to a stack, then pop them from stack to reverse them
+// Time: O(n) - two-pass
+// Space: O(n) - stack
+// 01/09/2018
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+        ListNode dummy = new ListNode(-1), p = dummy;
+        while (!stack.isEmpty()) {
+            p.next = stack.pop();
+            p = p.next;
+        }
+        p.next = null;
+        return dummy.next;
+    }
+}
+
