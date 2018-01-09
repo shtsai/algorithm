4eb26e946d3878efe0fd4e14803df3bcc9a3a89d
Shang-Hung Tsai <st3127@nyu.edu>
2018-01-09 10:37:23 -0500

    update Pow.java

--- java/Pow.java
+++ java/Pow.java
@@ -2,9 +2,30 @@
  * Implement pow(x, n).
  */
 
+// Solution 3: Recursion
+// Time: O(logn)
+// Space: O(logn)
+// 01/09/2018
+class Solution {
+    public double myPow(double x, int n) {
+        if (x == 0) return 0;
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = N * -1;
+        }
+        double singular = N % 2 == 1 ? x : 1;
+        N /= 2;
+        return myPow(x * x, (int) N) * singular;
+    }
+}
+
 // Solution 2:
 // Recursion: recursively divide problems into smaller subproblems
-// time: O(lg(n))
+// Time: O(lgn)
+// Space: O(lgn) - call stack
 public class Solution {
     public double myPow(double x, int n) {
         if (x == 0) return 0;
@@ -22,9 +43,10 @@ public class Solution {
     }
 }
 
-// Solution 1:
+// Solution 1: Brute force
 // Standard implementation
-// Time Limit Exceeded
+// Time: O(n) - Time Limit Exceeded
+// Space: O(1)
 public class Solution {
     public double myPow(double x, int n) {
         int sign = 1;       // need to handle negative n
