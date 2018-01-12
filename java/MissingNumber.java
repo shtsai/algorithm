1a3dee3e5e8d4932d6f6c4efc8014c58fae12153
Shang-Hung Tsai <st3127@nyu.edu>
2018-01-12 18:37:52 -0500

    update MissingNumber.java

--- java/MissingNumber.java
+++ java/MissingNumber.java
@@ -3,66 +3,59 @@
  * For example, Given nums = [0, 1, 3] return 2.
  */
 
-// XOR approach
+// Solution 3: XOR approach
 // for any number x, x ^ x = 0 and x ^ 0 = x
 // therefore, if we XOR all the indice and number together,
 // all the numbers will be cancelled out except the missing number
-
-public class Solution{
+// Time: O(n) - one pass
+// Space: O(1)
+// 01/12/2018
+class Solution {
     public int missingNumber(int[] nums) {
-        int len = nums.length;
-        int xor = len;
-        for (int i = 0; i<len; i++) {
-            xor = xor ^ i ^ nums[i];
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= i ^ nums[i];
         }
-        
-        return xor;
+        return res;
     }
 }
 
 
-/*
-// Sum approach
+// Solution 2: Sum approach
 // take the difference between a full array and a missing array
 // the result is the missing number
 // much faster than set approach
-
-public class Solution {
+// Time: O(n) - one pass
+// Space: O(1)
+// 01/12/2018
+class Solution {
     public int missingNumber(int[] nums) {
-        int len = nums.length;
-        int sum = len;
-        for (int i = 0; i < len; i++) {
-            sum = sum + i - nums[i];
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            res += i - nums[i];
         }
-        return sum;
+        return res;
     }
 }
-*/
 
 
-/*
-// Set approach slow because of two for loops
-public class Solution {
+// Solution 1: Set
+// Store all numbers into a HashSet.
+// Then scan from 0 - n to find the missing one.
+// Time: O(n) - two-pass
+// Space: O(n)
+// 01/12/2018
+class Solution {
     public int missingNumber(int[] nums) {
-
-        int len = nums.length;
-
-        Set<Integer> set = new HashSet<Integer>();
-
-        // first add all numbers to the set        
-        for (int i = 0; i<len; i++) {
-            set.add(nums[i]);
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
-
-        // then check if the set contains all numbers
-        // if not, return that number
-        for (int i = 0; i < len+1; i++) {
-            if (!set.contains(i)){
+        for (int i = 0; i <= nums.length; i++) {
+            if (!set.contains(i)) {
                 return i;
             }
         }
-        
-        return len+1;
+        return nums.length;
     }
 }
-*/
