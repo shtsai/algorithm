df089c4fe0c112570c97956dc5821992d9bdd76d
Shang-Hung Tsai <shtsai7@gmail.com>
2018-07-27 07:17:04 -0400

    add MaxIncreaseToKeepCitySkyline.java

---
+++ java/MaxIncreaseToKeepCitySkyline.java
@@ -0,0 +1,60 @@
+/*
+	In a 2 dimensional array grid, each value grid[i][j] represents the height of a building located there. We are allowed to increase the height of any number of buildings, by any amount (the amounts can be different for different buildings). Height 0 is considered to be a building as well. 
+
+	At the end, the "skyline" when viewed from all four directions of the grid, i.e. top, bottom, left, and right, must be the same as the skyline of the original grid. A city's skyline is the outer contour of the rectangles formed by all the buildings when viewed from a distance. See the following example.
+
+	What is the maximum total sum that the height of the buildings can be increased?
+
+	Example:
+	Input: grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+	Output: 35
+	Explanation: 
+	The grid is:
+	[ [3, 0, 8, 4], 
+	  [2, 4, 5, 7],
+	  [9, 2, 6, 3],
+	  [0, 3, 1, 0] ]
+
+	The skyline viewed from top or bottom is: [9, 4, 8, 7]
+	The skyline viewed from left or right is: [8, 7, 9, 3]
+
+	The grid after increasing the height of buildings without affecting skylines is:
+
+	gridNew = [ [8, 4, 8, 7],
+	            [7, 4, 7, 7],
+	            [9, 4, 8, 7],
+	            [3, 3, 3, 3] ]
+
+	Notes:
+
+	1 < grid.length = grid[0].length <= 50.
+	All heights grid[i][j] are in the range [0, 100].
+	All buildings in grid[i][j] occupy the entire grid cell: that is, they are a 1 x 1 x grid[i][j] rectangular prism.
+ */
+
+// Solution 1: Find max of rows and cols
+// Time: O(n^2)
+// Space: O(n)
+// 07/27/2018
+
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        // Get max of every row and column
+        int[] rowMaxs = new int[grid.length];
+        int[] colMaxs = new int[grid[0].length];
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                rowMaxs[r] = Math.max(rowMaxs[r], grid[r][c]);
+                colMaxs[c] = Math.max(colMaxs[c], grid[r][c]);
+            }
+        }
+        
+        int res = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                res += Math.min(rowMaxs[r], colMaxs[c]) - grid[r][c];
+            }
+        }
+        return res;
+    }
+}
