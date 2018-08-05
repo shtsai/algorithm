a5f25f386fdcf725cc68bb41ae7f8f92792db9f6
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-04 23:00:58 -0400

    add ProjectionAreaOf3DShapes.java

---
+++ java/ProjectionAreaOf3DShapes.java
@@ -0,0 +1,75 @@
+/*
+    On a N * N grid, we place some 1 * 1 * 1 cubes that are axis-aligned with the x, y, and z axes.
+
+    Each value v = grid[i][j] represents a tower of v cubes placed on top of grid cell (i, j).
+
+    Now we view the projection of these cubes onto the xy, yz, and zx planes.
+
+    A projection is like a shadow, that maps our 3 dimensional figure to a 2 dimensional plane. 
+
+    Here, we are viewing the "shadow" when looking at the cubes from the top, the front, and the side.
+
+    Return the total area of all three projections.
+
+
+    Example 1:
+
+    Input: [[2]]
+    Output: 5
+    Example 2:
+
+    Input: [[1,2],[3,4]]
+    Output: 17
+    Explanation: 
+    Here are the three projections ("shadows") of the shape made with each axis-aligned plane.
+
+    Example 3:
+
+    Input: [[1,0],[0,2]]
+    Output: 8
+    Example 4:
+
+    Input: [[1,1,1],[1,0,1],[1,1,1]]
+    Output: 14
+    Example 5:
+
+    Input: [[2,2,2],[2,1,2],[2,2,2]]
+    Output: 21
+     
+
+    Note:
+
+    1 <= grid.length = grid[0].length <= 50
+    0 <= grid[i][j] <= 50
+ */
+
+// Solution 1:
+// Count non-zero cells + max of each row and column
+// Time: O(n^2)
+// Space: O(1)
+// 08/04/2018
+
+class Solution {
+    public int projectionArea(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {    // side view
+            int max = 0;
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != 0) {  // top view
+                    res++;
+                }
+                max = Math.max(max, grid[i][j]);
+            }
+            res += max;
+        }
+        
+        for (int j = 0; j < grid[0].length; j++) {  // front view
+            int max = 0;
+            for (int i = 0; i < grid.length; i++) {
+                max = Math.max(max, grid[i][j]);
+            }
+            res += max;
+        }
+        return res;
+    }
+}
