2789023b1ca0de1e691a5622b003f53003998b54
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-12 21:09:03 -0400

    update WordSearch.java

--- java/WordSearch.java
+++ java/WordSearch.java
@@ -5,8 +5,51 @@
  * The same letter cell may not be used more than once.
  */
 
-public class Solution {
+// Solution 2: Recursive + visited 
+// Time: O(mn * 4 ^ s)
+// Space: O(mn)
+class Solution {
+    final int[][] dirs = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    boolean[][] visited = new boolean[board.length][board[0].length];
+                    if (search(board, word, 0, i, j, visited)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean search(char[][] board, String word, int index, int r, int c, boolean[][] visited) {
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || visited[r][c] || board[r][c] != word.charAt(index)) {
+            return false;
+        }
+        if (index == word.length() - 1) {
+            return true;
+        }
+        visited[r][c] = true;
+        for (int[] dir : dirs) {
+            if (search(board, word, index + 1, r + dir[0], c + dir[1], visited)) {
+                return true;
+            }
+        }
+        visited[r][c] = false;   // reset visited so that we can use this char later in another path
+        return false;
+    }
+}
 
+// Solution 1: Iterative + modify input
+// Time: O(mn * 4 ^ s)
+// Space: O(1)
+public class Solution {
     public boolean exist(char[][] board, String word) {
 	    int row = board.length;
 	    int col = board[0].length;
