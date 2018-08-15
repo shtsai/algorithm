cfaf16c4c2df7da267be157fb87a70dd5ec6b4a7
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-14 21:41:10 -0400

    update MergeIntervals.java

--- java/MergeIntervals.java
+++ java/MergeIntervals.java
@@ -1,6 +1,6 @@
 /*
  * Given a collection of intervals, merge all overlapping intervals.
- * 
+ *
  * For example,
  * Given [1,3],[2,6],[8,10],[15,18],
  * return [1,6],[8,10],[15,18].
@@ -20,7 +20,38 @@
 // Time: O(nlogn) - sorting + one passes
 // Space: O(1)
 
-// Version 3:
+// Version 3: lambda function
+// 08/14/2018
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        Collections.sort(intervals, (Interval a, Interval b) -> {
+            if (a.start == b.start) {
+                return a.end - b.end;
+            } else {
+                return a.start - b.start;
+            }
+        });
+
+        List<Interval> res = new ArrayList<>();
+        Interval prev = null;
+        for (Interval i : intervals) {
+            if (prev == null) {
+                prev = i;
+            } else if (prev.end >= i.start) {  // overlap
+                prev.end = Math.max(prev.end, i.end);
+            } else {
+                res.add(prev);
+                prev = i;
+            }
+        }
+        if (prev != null) {
+            res.add(prev);
+        }
+        return res;
+    }
+}
+
+// Version 2: anomynous comparator
 // 01/05/2018
 class Solution {
     public List<Interval> merge(List<Interval> intervals) {
@@ -52,39 +83,6 @@ class Solution {
     }
 }
 
-// version 2: use anonymous class
-// 11/17/2017
-class Solution {
-    public List<Interval> merge(List<Interval> intervals) {
-        Collections.sort(intervals, new Comparator<Interval> () {
-            @Override
-            public int compare(Interval a, Interval b) {
-                if (a.start == b.start) {
-                    return a.end - b.end;
-                }
-                return a.start - b.start;
-            }
-        });
-        List<Interval> res = new ArrayList<>();
-        if (intervals.size() == 0) return res;
-        Interval pre = intervals.get(0);
-        for (int i = 1; i < intervals.size(); i++) {
-            Interval current = intervals.get(i);
-            if (current.start <= pre.end) {
-                if (current.end <= pre.end) continue;
-                else {
-                    pre.end = current.end;
-                }
-            } else {
-                res.add(pre);
-                pre = current;
-            }
-        }
-        res.add(pre);
-        return res;
-    }
-}
-
 // version 1: write comparator class
 public class Solution {
     private class Cmp implements Comparator<Interval> {
@@ -95,7 +93,7 @@ public class Solution {
     public List<Interval> merge(List<Interval> intervals) {
         List<Interval> res = new ArrayList<>();
         if (intervals == null || intervals.size() == 0) return res;
-        
+
         Collections.sort(intervals, new Cmp());     // sort by accending start time
         int start = -1, end = -1;
         for (Interval i : intervals) {
@@ -112,7 +110,7 @@ public class Solution {
                 end = Math.max(end, i.end);
             }
         }
-        res.add(new Interval(start, end));      // add last interval  
+        res.add(new Interval(start, end));      // add last interval
         return res;
     }
 }
