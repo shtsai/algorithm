9c3a3981391a7694dc8aa6ead03c55fa1790c356
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-30 11:41:04 -0400

    update ReorderList.java

--- java/ReorderList.java
+++ java/ReorderList.java
@@ -17,12 +17,57 @@
  * }
  */
 
+// Solution 2:
 // Do the following three steps:
 // 1. Find the center of the List
 // 2. reverse the second half of the List
 // 3. Reorder the list to the interleaving order
-
 // reference: https://discuss.leetcode.com/topic/13869/java-solution-with-3-steps
+//
+// Time: O(n)
+// Space: O(1)
+
+// version 2:
+// 08/30/2018
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        ListNode fast = head, slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        ListNode tailHead = slow.next;
+        slow.next = null;               // break two parts
+        tailHead = reverse(tailHead);   // reverse second half
+        
+        ListNode p = head, q = tailHead;
+        while (p != null && q != null) {
+            ListNode pNext = p.next;
+            ListNode qNext = q.next;
+            p.next = q;
+            q.next = pNext;
+            p = pNext;
+            q = qNext;
+        }
+    }
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    
+}
+
+// version 1:
 class Solution {
     public void reorderList(ListNode head) {
         if (head == null) return;
@@ -54,5 +99,44 @@ class Solution {
             p2 = slow.next;
         }
         return;
+    }
+}
+
+// Solution 1: Two pointers
+// Store all nodes in an array
+// Use two pointer to mix up both end
+// 
+// Time: O(n)
+// Space: O(n)
+// 08/30/2018
+
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        List<ListNode> list = new ArrayList<>();
+        int len = 0;
+        while (head != null) {
+            list.add(head);
+            len++;
+            ListNode next = head.next;
+            head.next = null;       // break links
+            head = next;
+        }
+        ListNode prev = null;
+        int end = len % 2 == 0 ? len / 2 : len / 2 + 1;   // end length differ for odd and even
+        for (int i = 0; i < end; i++) {
+
+            ListNode left = list.get(i);
+            ListNode right = list.get(len - 1 - i);
+            if (prev != null) {
+                prev.next = left;
+            }
+            if (left != right) {    // odd case, last iteration, left == right
+                left.next = right;
+            }
+            prev = right;
+        }
     }
 }
