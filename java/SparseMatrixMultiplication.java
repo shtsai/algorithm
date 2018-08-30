747806b5e4c3efc469b11c5b8592f3cbcd8719b2
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-30 09:03:26 -0400

    update SparseMatrixMultiplication.java

--- java/SparseMatrixMultiplication.java
+++ java/SparseMatrixMultiplication.java
@@ -22,8 +22,51 @@
                       | 0 0 1 |
  */
 
-// Solution 2:
+// Solution 3: Store non-zero elements in HashMap
+// This approach works best if the matrix is sparse (mostly zeros)                
+// Time: O(mnl)
+// Space: O(mn + nl)
+// 08/30/2018
+
+class Solution {
+    public int[][] multiply(int[][] A, int[][] B) {
+        HashMap<Integer, HashMap<Integer, Integer>> tableA = new HashMap<>();
+        HashMap<Integer, HashMap<Integer, Integer>> tableB = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (A[i][j] != 0) {
+                    tableA.computeIfAbsent(i, x -> new HashMap<>()).put(j, A[i][j]);
+                }
+            }
+        }
+        
+        for (int i = 0; i < B.length; i++) {
+            for (int j = 0; j < B[0].length; j++) {
+                if (B[i][j] != 0) {
+                    tableB.computeIfAbsent(i, x -> new HashMap<>()).put(j, B[i][j]);
+                }
+            }
+        }
+        
+        int[][] res = new int[A.length][B[0].length];
+        for (Integer i : tableA.keySet()) {
+            for (Integer j : tableA.get(i).keySet()) {
+                if (!tableB.containsKey(j)) {
+                    continue;
+                }
+                for (Integer k : tableB.get(j).keySet()) {
+                    res[i][k] += tableA.get(i).get(j) * tableB.get(j).get(k);
+                }
+            }
+        }
+        return res;
+    }
+}
+
+// Solution 2:  early break
 // Check for '0', if '0' found, can skip several iterations
+// Time: O(mnl) - A m*n  B n*l
+// Space: O(1)                   
 // 09/27/2017
 class Solution {
     public int[][] multiply(int[][] A, int[][] B) {
@@ -47,7 +90,7 @@ class Solution {
 // Solution 1: Brute force solution
 // Time limit exceeded
 // Time: O(mnl) - A m*n  B n*l
-// Space: O(ml)
+// Space: O(1)
 // 09/27/2017
 
 class Solution {
