e679f73314978b9671fd73ba4be95e308826fada
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-30 10:30:24 -0400

    update RotateArray.java

--- java/RotateArray.java
+++ java/RotateArray.java
@@ -3,14 +3,15 @@
  * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
  */
 
-public class Solution {
+// Solution 3: Three reverses
+// first reverse the whole array
+// then the first half up to k
+// lastly, reverse the rest of the array 
+// Time: O(n)
+// Space: O(1)
 
-    // Three reverse calls
-    // O(n) time, O(1) space
+public class Solution {
     public void rotate(int[] nums, int k) {
-        // first reverse the whole array
-        // then the first half up to k
-        // lastly, reverse the rest of the array 
         int len = nums.length;
         k = k % len;
         reverse(nums, 0, len-1);
@@ -28,11 +29,32 @@ public class Solution {
             i++;
         }
     }
+}
 
-    /*  
-    // Store and rotate
-    // O(n) time, O(k) space
-    // store the last k elements, and then rotate
+// Solution 2: 
+// Rotate the array in a temp array
+// Time: O(n)
+// Space: O(n)
+// 08/30/2018
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            temp[(i + k) % nums.length] = nums[i];
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
+
+// Solution 1: Store and rotate
+// Store and rotate
+// Time: O(n) 
+// Space: O(k) 
+// store the last k elements, and then rotate
+class Solution {
     public void rotate(int[] nums, int k) {
         int len = nums.length;
         k = k % len;
@@ -48,5 +70,4 @@ public class Solution {
             nums[i] = temp[i];
         }
     }
-    */
 }
