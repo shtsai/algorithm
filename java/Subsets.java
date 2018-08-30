0d810652050b3e715d1a2a8dd3855da3c9041d08
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-30 14:57:01 -0400

    update Subsets.java

--- java/Subsets.java
+++ java/Subsets.java
@@ -18,33 +18,10 @@
  * ]
  */
 
-// Solution 2:
-// iterative through each integer in the array
-// two possible choices: add it to the set, or not to add it
-// recursively making such choices and add to the result when we have gone over all integers
-// time: O(2^n)
-class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        helper(nums, 0, new ArrayList<Integer>(), res);
-        return res;
-    }
-    private void helper (int[] nums, int index, List<Integer> list, List<List<Integer>> res) {
-        if (index == nums.length) { // we have gone over all integers at this point
-            res.add(new ArrayList<Integer>(list));
-            return;
-        }
-        list.add(nums[index]);        // to include this int in the subset
-        helper(nums, index+1, list, res);
-        list.remove(list.size()-1);   // not to include it
-        helper(nums, index+1, list, res);
-    }
-}
-
-
-// Solution 1: version 2
+// Solution 2: version 2
 // backtracking
-// time: O(n!)
+// Time: O(2^n)
+// Space: O(n)
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
@@ -68,22 +45,27 @@ public class Solution {
     }
 }
 
-// Solution 1: version 1
-// need to add empty subset manually
+
+// Solution 1:
+// Iterate through each integer in the array
+// Two possible choices: add it to the set, or not to add it
+// Recursively making such choices and add to the result when we have gone over all integers
+// Time: O(2^n)
+// Space: O(n) - stack space
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList<>());     // empty subset
-        helper(nums, res, 0, new ArrayList<Integer>());
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(nums, 0, new ArrayList<Integer>(), res);
         return res;
     }
-
-    private void helper(int[] nums, List<List<Integer>> res, int index, List<Integer> list) {
-        for (int i = index; i < nums.length; i++) {
-            list.add(nums[i]);
+    private void helper (int[] nums, int index, List<Integer> list, List<List<Integer>> res) {
+        if (index == nums.length) { // we have gone over all integers at this point
             res.add(new ArrayList<Integer>(list));
-            helper(nums, res, i+1, list);
-            list.remove(list.size()-1);
+            return;
         }
+        list.add(nums[index]);        // to include this int in the subset
+        helper(nums, index+1, list, res);
+        list.remove(list.size()-1);   // not to include it
+        helper(nums, index+1, list, res);
     }
 }
