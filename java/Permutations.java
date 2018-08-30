b104c7eeee4b939e1a64a8101feb700318bef243
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-30 15:51:20 -0400

    update Permutations.java

--- java/Permutations.java
+++ java/Permutations.java
@@ -8,8 +8,6 @@
 // reference: https://leetcode.com/problems/permutations/discuss/18237/
 // Time: O(n!): n! permutations
 // Space O(n * n!): n! permutations, each has length of n
-
-// version 2:
 // 01/15/2018
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
@@ -29,30 +27,6 @@ class Solution {
         return res;
     }
 }
-
-// version 1: need initialization
-public class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return res;
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(nums[0]);
-        res.add(list);
-        for (int i = 1; i < nums.length; i++) {     // for every number
-            List<List<Integer>> newRes = new ArrayList<>();
-            for(int j = 0; j <= i; j++) { // for every insert position
-                for (List<Integer> l : res) {
-                    List<Integer> newL = new ArrayList<Integer>(l);
-                    newL.add(j, nums[i]);
-                    newRes.add(newL);
-                }
-            }
-            res = newRes;
-        }
-        return res;
-    }
-}
-
 
 // Solution 2:
 // first convert int[] to ArrayList so that we can add and remove easily
