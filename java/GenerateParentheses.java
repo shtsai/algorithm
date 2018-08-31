74cf8e58e4acd56f1123ec5a73393ac1289f790a
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-30 21:58:47 -0400

    update GenerateParentheses.java

--- java/GenerateParentheses.java
+++ java/GenerateParentheses.java
@@ -2,7 +2,36 @@
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
  */
 
-// backtracking
+// Solution 1:
+// Keep track of three variables:
+//     left, right, and stack
+// Recursively add left and right,
+// Break when invalid
+//
+// Time: O(2^n)
+// Space: O(n)
+// 08/30/2018
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        generate(res, n, n, 0, "");
+        return res;
+    }
+    
+    private void generate(List<String> res, int left, int right, int stack, String current) {
+        if (left < 0 || right < 0 || stack < 0) {
+            return;
+        } else if (left == 0 && right == 0 && stack == 0) {
+            res.add(current);
+            return;
+        } else {
+            generate(res, left - 1, right, stack + 1, current + "(");
+            generate(res, left, right - 1, stack - 1, current + ")");
+        }
+    }
+}
+
+// version 2:
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> list = new ArrayList<String>();
