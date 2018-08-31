7af6169b1fae7422e6631120d12d97c01c41e7ee
Shang-Hung Tsai <shtsai7@gmail.com>
2018-08-31 15:34:14 -0400

    update WordLadder.java

--- java/WordLadder.java
+++ java/WordLadder.java
@@ -131,10 +131,8 @@ class Solution {
             count++;    // reached a new level
             if (newReached.contains(endWord)) return count;
             reached = newReached;
-        }
-        
+        }   
         return count;
-        
     }
     
     // a helper function that checks if two strings represents a valid transformation
@@ -145,56 +143,5 @@ class Solution {
             if (a.charAt(i) != b.charAt(i)) diff++;
         }
         return diff == 1;
-    }
-}
-
-
-/* -------------OLD VERSION, PROBLEM IS UPDATED------------------- */
-
-/*
- * Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
- * Only one letter can be changed at a time
- * Each intermediate word must exist in the word list
- * For example,
- *
- * Given:
- * beginWord = "hit"
- * endWord = "cog"
- * wordList = ["hot","dot","dog","lot","log"]
- * As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
- * return its length 5.
-*/
-
-// transform the problem into a graph
-// we are trying to reach the destination from the source
-// use BFS to explore all reachable nodes step by step
-public class Solution {
-    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
-        HashSet<String> reached = new HashSet<>();
-        int steps = 1;
-        reached.add(beginWord);         // reached nodes
-        wordList.add(endWord);          // unreached notes
-        
-        while (!reached.contains(endWord)) { 
-            HashSet<String> newReached = new HashSet<>();     // a new set to keep track of the newly reached nodes
-            for (String word : reached) {
-                for (int i = 0; i < word.length(); i++) {  // try all positions
-                    char[] chars = word.toCharArray();
-                    for (char x = 'a'; x <= 'z'; x++) {    // try all possible characters
-                        chars[i] = x;
-                        String newWord = new String(chars);
-                        if (wordList.contains(newWord)) {  // reached a new node, add to the list
-                            newReached.add(newWord);
-                            wordList.remove(newWord);
-                        }
-                    }
-                }
-            }
-            steps++;        // use one step in each iteration of the loop
-            if (newReached.size() == 0) return 0;  // no way to reach new nodes, so no way to reach destination
-            reached = newReached;   // update the reached set, now expand from the newly reached nodes
-        }
-        
-        return steps;
     }
 }
