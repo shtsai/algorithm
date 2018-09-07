af5aab1c070c344371cf1d81c983d29533245f47
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-07 16:06:06 -0400

    update TwoSum.java

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -11,8 +11,11 @@
  *  
  */
 
-// Improved version solution 1:
-// combine two for loops
+// Solution 1:
+// use HashMap to store nums for fast look up
+// Time: O(n)
+// Space: O(n)
+// 09/07/2018
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
@@ -24,26 +27,6 @@ class Solution {
                 res[1] = map.get(target-nums[i]);
             } else {
                 map.put(nums[i], i);
-            }
-        }
-        return res;
-    }
-}
-
-// Solution 1:
-// use HashMap to store nums for fast look up
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] res = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        
-        for (int i = 0; i < nums.length; i++) {     // cannot use the same num twice
-            if (map.containsKey(target-nums[i]) && map.get(target-nums[i]) != i) {
-                res[0] = i;
-                res[1] = map.get(target-nums[i]);
             }
         }
         return res;
