6f59aceed7829b97bf286117ce2b2e8ef4dc8e7d
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-08 18:34:10 -0400

    update KthLargestElementInAnArray.java

--- java/KthLargestElementInAnArray.java
+++ java/KthLargestElementInAnArray.java
@@ -5,7 +5,6 @@
  * Given [3,2,1,5,6,4] and k = 2, return 5.
  */
 
-
 // Solution 2: Selection algorithm
 // Use the idea of partition and pivot that is similiar to quick sort
 // Some binary search flavour here.
@@ -13,7 +12,7 @@
 //       Can use randomization to get good average runtime
 // Space: O(1)
 // reference: https://discuss.leetcode.com/topic/14597/solution-explained
-// version 2: make partition() a little more concise
+// 09/08/2018
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         k = nums.length - k;
@@ -34,9 +33,15 @@ class Solution {
         int pivot = left, value = nums[pivot];
         int l = left + 1, r = right;
         while (l <= right && r > left) {
-            while (l <= right && nums[l] <= value) l++;
-            while (r > left && nums[r] > value) r--;
-            if (l > r) break;
+            while (l <= right && nums[l] <= value) {
+                l++;
+            }
+            while (r > left && nums[r] > value) {
+                r--;
+            }
+            if (l > r) {
+                break;
+            }
             swap(nums, l, r);
         }
         swap(nums, pivot, r);
@@ -49,92 +54,22 @@ class Solution {
     }
 }
 
-
-// version 1:
-// 09/22/2017
-class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        k = nums.length - k;
-        int low = 0, high = nums.length-1;
-        while (low < high) {
-            int index = partition(nums, low, high);
-            if (index == k) {   // it is the kth 
-                break;
-            } else if (index > k) {
-                high = index-1;
-            } else {
-                low = index+1;
-            }
-        }
-        return nums[k];     // the Kth largest number that be moved to its correct position
-    }
-    
-    /* This function uses number at index low as the pivot,
-       move it to its sorted position, and make sure numbers
-       on its left are smaller and numbers on its right are
-       larger.                                          */    
-    private int partition(int[] nums, int low, int high) {
-        int i = low + 1, j = high, pivot = nums[low];
-        while (true) {
-            while (i < high) {
-                if (nums[i] > pivot) {  // find the number on the left part that is greater than the pivot
-                    break;
-                } else {
-                    i++;
-                }
-            }
-            while (j > low) {
-                if (nums[j] < pivot) {  // find the number on the right part that is less than the pivot
-                    break;
-                } else {
-                    j--;
-                }
-            }
-            if (i >= j) break;  // invalid indices
-            swap(nums, i, j);
-        }
-        swap(nums, j, low);     // low is the index of the pivot, move it to its correct position
-        return j;   // return the ranking of pivot
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-}
-
-// improved version of solution 1:
-class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        for (int i = 0; i < nums.length; i++) {
-            q.offer(nums[i]);
-            if (q.size() > k) {
-                q.poll();
-            }
-        }
-        return q.peek();
-    }
-}
-// Solution 1:
-// use a priority queue of size k to keep the largest k numbers
+// Solution 1: Min heap
+// Use a priority queue of size k to keep the largest k numbers
 // Time: O(nlogk)
 // Space: O(k) - priority queue
-public class Solution {
+// 09/08/2018
+class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (pq.size() < k) {       // if the priority queue is not full
-                pq.offer(nums[i]);     // add to the queue
-            } else {
-                int min = pq.peek();   // otherwise, get the min
-                if (min < nums[i]) {   // and compare to the current element, if min is smaller
-                    pq.poll();         // add the current element to the queue
-                    pq.offer(nums[i]);
-                }
+        for (int n : nums) {
+            if (pq.size() < k) {      
+                pq.offer(n);
+            } else if (pq.size() == k && pq.peek() < n) {
+                pq.poll();
+                pq.offer(n);
             }
         }
-        return pq.peek();   // return the smallest element in the queue
+        return pq.peek(); 
     }
 }
