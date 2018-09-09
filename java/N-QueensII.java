7d6675912726c72f9290cef94a90efd90ded9892
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-09 15:46:44 -0400

    update N-QueensII.java

--- java/N-QueensII.java
+++ java/N-QueensII.java
@@ -4,10 +4,48 @@
  * Now, instead outputting board configurations, return the total number of distinct solutions.
  */
 
+// Solution 2: 
+// No need to actually build the board, just check if col, diagonals conflicts.
+// Time: O(n ^ n)
+// Space: O(n)
+// 09/09/2018
+class Solution {
+    int count;
+    public int totalNQueens(int n) {
+        count = 0;
+        boolean[] cols = new boolean[n];
+        boolean[] diag1 = new boolean[n * 2];
+        boolean[] diag2 = new boolean[n * 2];
+        search(0, n, cols, diag1, diag2);
+        return count;
+    }
+    
+    private void search(int row, int n, boolean[] cols, boolean[] diag1, boolean[] diag2) {
+        if (row == n) {
+            count++;
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            int d1 = col - row + n;
+            int d2 = col + row;
+            
+            if (cols[col] || diag1[d1] || diag2[d2]) {
+                continue;
+            }
+            cols[col] = true;
+            diag1[d1] = true;
+            diag2[d2] = true;
+            search(row + 1, n, cols, diag1, diag2);
+            cols[col] = false;
+            diag1[d1] = false;
+            diag2[d2] = false;
+        }
+    }
+}
+
 // Solution 1: DFS, backtracking
 // Time: O(n^n) - n choices at n levels
 // Space: O(n) - stack
-
 // version 2:
 // 01/31/2018
 class Solution {
