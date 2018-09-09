d0176b09e92384014a223afd34fed8ec137a20de
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-09 10:03:51 -0400

    add OnlineStockSpan.java

---
+++ java/OnlineStockSpan.java
@@ -0,0 +1,82 @@
+/*
+	Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current day.
+
+	The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
+
+	For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
+
+	Example 1:
+	Input: ["StockSpanner","next","next","next","next","next","next","next"], [[],[100],[80],[60],[70],[60],[75],[85]]
+	Output: [null,1,1,1,2,1,4,6]
+	Explanation: 
+	First, S = StockSpanner() is initialized.  Then:
+	S.next(100) is called and returns 1,
+	S.next(80) is called and returns 1,
+	S.next(60) is called and returns 1,
+	S.next(70) is called and returns 2,
+	S.next(60) is called and returns 1,
+	S.next(75) is called and returns 4,
+	S.next(85) is called and returns 6.
+
+	Note that (for example) S.next(75) returned 4, because the last 4 prices
+	(including today's price of 75) were less than or equal to today's price.
+	 
+	Note:
+	Calls to StockSpanner.next(int price) will have 1 <= price <= 10^5.
+	There will be at most 10000 calls to StockSpanner.next per test case.
+	There will be at most 150000 calls to StockSpanner.next across all test cases.
+	The total time limit for this problem has been reduced by 75% for C++, and 50% for all other languages.
+ */
+
+// Solution 2: Stack
+// Maintain a stack by decreasing prices
+// Compress days of periods of increasing prices.
+// Time: O(q) - q: # of queries
+// Space: O(q)
+// 09/09/2018
+class StockSpanner {
+    Stack<Integer> prices;
+    Stack<Integer> weights;
+    
+    public StockSpanner() {
+        prices = new Stack<>();
+        weights = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int w = 1;
+        while (!prices.isEmpty() && prices.peek() <= price) {
+            prices.pop();
+            w += weights.pop();
+        }
+        prices.push(price);
+        weights.push(w);
+        return w;
+    }
+}
+
+// Solution 1: Count continuous days that is lower than or equal to current price
+// Time: O(n) - Time limit exceeded
+// Space: O(n)
+// 09/09/2018
+
+class StockSpanner {
+    List<Integer> prices;
+    
+    public StockSpanner() {
+        prices = new ArrayList<>();
+    }
+    
+    public int next(int price) {
+        int res = 1;
+        for (int i = prices.size() - 1; i >= 0; i--) {
+            if (prices.get(i) <= price) {
+                res++;
+            } else {
+                break;
+            }
+        }
+        prices.add(price);
+        return res;
+    }
+}
