db76d4434ace5e2cd0752b0b2cebf9333a833d75
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-09 17:09:24 -0400

    update SearchInRotatedSortedArray.java

--- java/SearchInRotatedSortedArray.java
+++ java/SearchInRotatedSortedArray.java
@@ -19,21 +19,27 @@
 // 
 // Time: O(log n)
 // Space: O(1)
-// 10/13/2017
-
-public class Solution {
+// 09/09/2018
+class Solution {
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) return -1;
         int i = 0, j = nums.length - 1;
         while (i < j) {
             int mid = i + (j - i) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] >= nums[i]) {       // left half is in accending order
-                if (nums[i] <= target && target <= nums[mid]) j = mid - 1;
-                else i = mid + 1;
-            } else {                               // right half is in accending order
-                if (nums[mid] <= target && target <= nums[j]) i = mid + 1;
-                else j = mid - 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[i]) {       // left half is in accending order
+                if (nums[i] <= target && target <= nums[mid]) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            } else {                                // right half is in accending order
+                if (nums[mid] <= target && target <= nums[j]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                } 
             }
         }
         return nums[i] == target ? i : -1;
