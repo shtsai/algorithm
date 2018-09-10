842e03fdf38d1119994e8f6b741befa90a65bf40
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-09 21:41:28 -0400

    update LongestValidParentheses.java

--- java/LongestValidParentheses.java
+++ java/LongestValidParentheses.java
@@ -12,26 +12,22 @@
 // reference: https://discuss.leetcode.com/topic/2426/my-dp-o-n-solution-without-using-stack
 // time: O(n) one pass
 // space: O(n)
-
+// 09/09/2018
 class Solution {
     public int longestValidParentheses(String s) {
-        if (s == null || s.length() <= 1) return 0;
-        int[] longest = new int[s.length()];    // stores the longest valid parentheses ended at index i
-        int max = Integer.MIN_VALUE;
-
+        int[] dp = new int[s.length()];
+        int max = 0;
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) == ')') {
-                if (s.charAt(i-1) == '(') {
-                    longest[i] = (i - 2 >= 0) ? longest[i-2] + 2 : 2;
-                } else {      // s.charAt(i-1) == ')'
-                    if (i - longest[i-1]-1 >= 0 && s.charAt(i-longest[i-1]-1) == '(') {
-                        longest[i]=longest[i-1] + 2 + (i-longest[i-1]-2>=0 ? longest[i-longest[i-1]-2] : 0);
-                    }
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i - 2 >= 0 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] - 1 >= 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0) + dp[i - 1] + 2;
                 }
-                max = Math.max(max, longest[i]);
-            }   // no need to check for '(', b/c it must be 0
+                max = Math.max(max, dp[i]);
+            }
         }
-        return max == Integer.MIN_VALUE ? 0 : max;
+        return max;
     }
 }
 
