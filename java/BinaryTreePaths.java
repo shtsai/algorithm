08a31f7fd1fcbad69c9f7f410e912da0adbf564a
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-11 16:14:13 -0400

    update BinaryTreePaths.java

--- java/BinaryTreePaths.java
+++ java/BinaryTreePaths.java
@@ -1,5 +1,19 @@
 /*
- * Given a binary tree, return all root-to-leaf paths.
+    Given a binary tree, return all root-to-leaf paths.
+
+    Note: A leaf is a node with no children.
+
+    Example:
+    Input:
+
+       1
+     /   \
+    2     3
+     \
+      5
+
+    Output: ["1->2->5", "1->3"]
+    Explanation: All root-to-leaf paths are: 1->2->5, 1->3
  */
 
 /**
@@ -12,63 +26,97 @@
  * }
  */
 
-// Solution 1 version 2: DFS
-// add "->" right after each value, except for the leaf
-// Much cleaner
+// Solution 3: DFS + stack
 // Time: O(n)
-// Space: O(logn) - call stack
-
-public class Solution {
+// Space: O(logn)
+// 09/11/2018
+class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new ArrayList<>();
-        if (root == null) return result;
-        helper(result, "" + root.val, root);  //put in the root value
-        return result;
-    }
-    
-    public void helper(List<String> result, String s, TreeNode root) {
-        if (root.left == null && root.right == null) { // the current node is a left
-            result.add(s);  // add the string to the result
-            return;
+        if (root == null) {
+            return new ArrayList<>();
         }
-        if (root.left != null) {
-            helper(result, s + "->" + root.left.val, root.left); // recursively solve left
+        List<String> res = new ArrayList<>();
+        Stack<TreeNode> parents = new Stack<>();
+        Stack<String> strings = new Stack<>();
+        parents.push(root);
+        strings.push(Integer.toString(root.val));        
+            
+        while (!parents.isEmpty()) {
+            TreeNode node = parents.pop();
+            String cur = strings.pop();
+            if (node.left == null && node.right == null) {
+                res.add(cur);
+            } 
+            if (node.right != null) {
+                parents.push(node.right);
+                strings.push(cur + "->" + node.right.val);
+            }
+            if (node.left != null) {
+                parents.push(node.left);
+                strings.push(cur + "->" + node.left.val);
+            }
         }
-        if (root.right != null) {
-            helper(result, s + "->" + root.right.val, root.right); // recursively solve right
-        }
-    }
+        return res;
+    }    
 }
 
-// Solution 1: DFS and StringBuilder
-// 10/31/2017
+// Solution 2: BFS + queue
+// Use two queues to store next node and corresponding string
+// Time: O(n)
+// Space: O(logn)
+// 09/11/2018
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<String> res = new ArrayList<>();
+        Queue<TreeNode> parents = new LinkedList<>();
+        Queue<String> strings = new LinkedList<>();
+        parents.offer(root);
+        strings.offer(Integer.toString(root.val));        
+            
+        while (!parents.isEmpty()) {
+            TreeNode node = parents.poll();
+            String cur = strings.poll();
+            if (node.left == null && node.right == null) {
+                res.add(cur);
+            } else {
+                if (node.left != null) {
+                    parents.offer(node.left);
+                    strings.offer(cur + "->" + node.left.val);
+                }
+                if (node.right != null) {
+                    parents.offer(node.right);
+                    strings.offer(cur + "->" + node.right.val);
+                }
+            }
+        }
+        
+        return res;
+    }    
+}
+
+// Solution 1: DFS
+// add "->" right after each value, except for the leaf
+// Time: O(n)
+// Space: O(logn) - call stack
+// 09/11/2018
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList<>();
-        if (root == null) return res;
-        
-        StringBuilder sb = new StringBuilder();
-        dfs(root, sb, res, true);        
+        dfs(root, res, "");
         return res;
     }
     
-    private void dfs(TreeNode node, StringBuilder sb, List<String> res, boolean isRoot) {
-        int preLen = sb.length();
-        if (isRoot) {
-            sb.append(node.val);
+    private void dfs(TreeNode node, List<String> res, String s) {
+        if (node == null) {
+            return;
+        } else if (node.left == null && node.right == null) {
+            res.add(new String(s + node.val));            
         } else {
-            sb.append("->" + node.val);
+            dfs(node.left, res, s + node.val + "->");
+            dfs(node.right, res, s + node.val + "->");
         }
-        
-        if (node.left == null && node.right == null) {  // find a leaf
-            res.add(new String(sb));
-        }
-        if (node.left != null) {
-            dfs(node.left, sb, res, false);
-        }
-        if (node.right != null) {
-            dfs(node.right, sb, res, false);
-        }
-        sb.delete(preLen, sb.length());
     }
 }
