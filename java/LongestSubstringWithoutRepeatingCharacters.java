8ff4b800588f2be8e5ae231facf7bdcea0598465
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-11 22:55:01 -0400

    update LongestSubstringWithoutRepeatingCharacters.java

--- java/LongestSubstringWithoutRepeatingCharacters.java
+++ java/LongestSubstringWithoutRepeatingCharacters.java
@@ -10,7 +10,29 @@
  * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 
-// Solution 2: two pointers and HashSet
+// Solution 2: One pass + HashMap
+// Store the last occurrence index for the duplicate char
+//
+// Time: O(n) - one pass
+// Space: O(n) - HashMap
+// 09/11/2018
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(left, map.get(s.charAt(right)));
+            }
+            max = Math.max(max, right - left + 1);
+            map.put(s.charAt(right), right + 1);
+        }
+        return max;
+    }
+}
+
+// Solution 1: Sliding window + HashSet
 // Use a HashSet to keep track of the characters that appears
 // in current window.
 // At the start of each iteration, we check if the new char is
@@ -36,33 +58,6 @@ class Solution {
             right++;
             max = Math.max(right-left, max);
         }
-        return max;
-    }
-}
-
-// Solution 1: HashMap and last appeared index
-public class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int start = 0, end = 0, len = 0, max = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        
-        while (end < s.length()) {
-            char c = s.charAt(end);
-            if (!map.containsKey(c)) {  // find a non-repeating character
-                map.put(c, end);        // record the last appeared index
-                len++;
-                max = Math.max(max, len);       // update max
-            } else {    // find a repeating character
-                for (int i = start; i < map.get(c); i++) {
-                    map.remove(s.charAt(i));    // remove chars that are no longer part of the substring
-                }
-                start = map.get(c) + 1;     // move the start pointer to new start point
-                len = end - start + 1;
-                map.put(c, end);
-            }
-            end++;
-        }
-        
         return max;
     }
 }
