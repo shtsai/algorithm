7caadbe62b48204b5322cba683f9985ab049975b
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-12 20:58:59 -0400

    update ConstructBinaryTreeFromInorderAndPostorderTraversal.java

--- java/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ java/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -15,7 +15,11 @@
  * }
  */
 
+// Solution 1:
 // Similar to (105) Construct Binary Tree from Preorder and Inorder Traversal
+// Time: O(n)
+// Space: O(n) - stack
+
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         if (inorder.length == 0 || postorder.length == 0) return null;
@@ -26,7 +30,9 @@ class Solution {
         if (inStart > inEnd || inStart >= postorder.length) return null;
         int rootIndex = inStart;
         // postorder[postStart] is the root, rootIndex is the index of root in inorder array
-        while (rootIndex <= inEnd && inorder[rootIndex] != postorder[postStart]) rootIndex++;     
+        while (rootIndex <= inEnd && inorder[rootIndex] != postorder[postStart]) {
+            rootIndex++;     
+        }
         TreeNode root = new TreeNode(postorder[postStart]);
         // (inEnd - rootIndex) = number of nodes in the right subtree
         // postStart-(inEnd-rootIndex)-1 gives you the last node of the left subtree, which is its root
