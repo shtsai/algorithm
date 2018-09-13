eab93023712be822f6fa7618bdf7908c0f3db26b
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-12 21:25:04 -0400

    update MaximumProductSubarray.java

--- java/MaximumProductSubarray.java
+++ java/MaximumProductSubarray.java
@@ -7,6 +7,8 @@
 
 // Solution 2:
 // dynamic programming
+// Time: O(n)
+// Space: O(1)
 public class Solution {
     public int maxProduct(int[] nums) {
         int max = nums[0], min = nums[0], result = nums[0];
@@ -17,15 +19,18 @@ public class Solution {
             // update max and min
             max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
             min = Math.min(Math.min(max2 * nums[i], min * nums[i]), nums[i]);
-            if (max > result) result = max;
+            if (max > result) {
+                result = max;
+            }
         }
         
         return result;
     }
 }
 
-// Solution 1:
-// brute force
+// Solution 1: brute force
+// Time: O(n ^ 2)
+// Space: O(1)
 public class Solution {
     public int maxProduct(int[] nums) {
         if (nums.length == 1) return nums[0];
@@ -35,7 +40,9 @@ public class Solution {
             if (product > max) max = product;
             for (int j = i + 1; j < nums.length; j++) {
                 product *= nums[j];
-                if (product > max) max = product;
+                if (product > max) {
+                    max = product;
+                }
             }
         }
         return max;
