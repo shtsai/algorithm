ad4338103bc939eda344dc597e09f0f7ce607481
Shang-Hung Tsai <shtsai7@gmail.com>
2018-09-22 20:34:01 -0400

    update DailyTemperatures.java

--- java/DailyTemperatures.java
+++ java/DailyTemperatures.java
@@ -6,6 +6,30 @@
 	Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
  */
 
+// Solution 2: Stack
+// Maintain a decreasing stack
+// When see a higher value, we can then add next warmer day.
+// Time: O(n)
+// Space: O(n)
+// 09/22/2018
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                res[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            res[stack.pop()] = 0;
+        }
+        return res;
+    }
+}
+
 // Solution 1: Brute force
 // Time: O(n^2)
 // Space: O(1)
